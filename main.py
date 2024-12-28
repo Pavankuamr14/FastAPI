4d4cb5b195df0c58274440c1261539beaf2e615b
pavankuamr14 <pavankumar14182@gmail.com>
2024-12-28 22:01:50 +0530

    adding demostration code

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app=FastAPI()
 @app.get('/')
 def root():
   return {"message":"welcome to fastapi learning course"}
+
+########################
+@app.get('/')
+def demo():
+  return {"message":"This is first msg need to be display"}
+
+#############################
+@app.get('/login')
+def loginfunction():
+  return {"Message":"welcome to login route page.."}
