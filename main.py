28d456d2f1266b20132afa06b4a1ee97f5076238
pavankuamr14 <pavankumar14182@gmail.com>
2024-12-29 09:00:39 +0530

    creating post function and using Basemodel

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
-app=FastAPI()
+app = FastAPI()
 
-@app.get('/')
+class Post(BaseModel):
+  tittle:str
+  context:str
+@app.get("/")
 def root():
-  return {"message":"welcome to fastapi learning course"}
+    return {"message": "welcome to fastapi learning course"}
+
 
 ########################
-@app.get('/')
+@app.get("/")
 def demo():
-  return {"message":"This is first msg need to be display"}
+    return {"message": "This is first msg need to be display"}
+
 
 #############################
-@app.get('/login')
+@app.get("/login")
 def loginfunction():
-  return {"Message":"welcome to login route page.."}
+    return {"Message": "welcome to login route page.."}
+
+
+# @app.post("/createPost")
+# def createpost():
+#     return {"New_user": "newuser is successfully created"}
+
+
+# @app.post('/createPost')
+# def createpost(payloads:dict =Body(...)):
+#   print(payloads)
+
+#   return {"New_Post":f"{payloads['tittle']} New_Post is created successfully And content is {payloads['context']}"}
+@app.post('/createPost')
+def createpost(New_Post:Post):
+  print(payloads)
+
+  return {"New_Post":f"{payloads['tittle']} New_Post is created successfully And content is {payloads['context']}"}
