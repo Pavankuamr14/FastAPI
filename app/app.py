d10685946172b6b3e9cadaef02d2613a5dee9bc9
pavankuamr14 <pavankumar14182@gmail.com>
2025-01-07 14:07:08 +0530

    connecting to postgres database
    implemented all the curd operation with the sql using the psycog2 libary and using the cursor.execute command performed all the curd operation with the database using the postman.

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@
 #   title:str
 #   content:str
 
-  
+
 # temporaryDataBase =[]
 # def findData(id_value):
 #   for data in temporaryDataBase:
@@ -45,14 +45,8 @@
 #   data['id']=id
 #   temporaryDataBase[data]=postdata
 #   return {'message':f"your are successfuly {postdata}"}
-  
-  
-  
-  
 
-  
-  
-  
+
 # # from fastapi import FastAPI, status
 # # from pydantic import BaseModel
 # # from random import randrange
@@ -80,49 +74,87 @@
 ### CURD methods
 
 # C- create
-# U - Update 
-  #   PUT
-  #   PATCH
+# U - Update
+#   PUT
+#   PATCH
 # R - Read - one post or all post
 # D - delete
 from fastapi import FastAPI, status, HTTPException
+import psycopg2.extras
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title: Optional[str]
-    content: Optional[str]
 
-temporaryDataBase = []
+class Post(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool] = True
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="pavan@5701",
+            cursor_factory=psycopg2.extras.RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was successfull")
+        break
+    except Exception as error:
+        print("connection to database error")
+        print("Error: ", error)
+        time.sleep(2)
+
+
+temporaryDataBase = [{'title':'first title','content':'my first post title',id:1}]
+
 
 def findDataIndex(id_value):
     for index, data in enumerate(temporaryDataBase):
-        if data['id'] == id_value:
+        if data["id"] == id_value:
             return index
     return None
 
-@app.get('/')
+
+@app.get("/")
 def root():
     return {"details": temporaryDataBase}
 
-@app.get('/posts/{id}')
+@app.get('/posts')
+def get_posts():
+    cursor.execute("""SELECT * FROM posts""") 
+    posts = cursor.fetchall()   
+    return {'data':posts}                   # Return the fetched data
+
+
+@app.get("/posts/{id}")
 def getdatabyID(id: int):
-    for data in temporaryDataBase:
-        if data['id'] == id:
-            return {"detailed message": data}
-    raise HTTPException(status_code=404, detail="Data not found")
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id)))
+    post=cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=404, detail=f"post with id : {id} Data was not found")
+    return {"post_details":post}
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def createPost(post: Post):
-    postData = post.dict()
-    postData['id'] = randrange(1, 100000000000)
-    temporaryDataBase.append(postData)
-    return {"created data": postData}
+    # cursor.execute(f"{INSERT INTO posts (title,content,published) VALUES (post.title,post.content,post.published)}") --THIS WILL MAY CAUSE THE SQL ingestion TO AVOID THIS WE WILL USE THE BELOW COMMAND
+    
+    cursor.execute("""INSERT INTO posts(title,content,published) VALUES (%s,%s,%s) RETURNING * """,
+                   (post.title,post.content,post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"created data": new_post}
 
-@app.patch('/posts/{id}')
+@app.patch("/posts/{id}")
 def updatePartialById(id: int, post: Post):
     index = findDataIndex(id)  # Find the index of the post with the given ID
     if index is None:
@@ -135,12 +167,26 @@ def updatePartialById(id: int, post: Post):
     temporaryDataBase[index] = updated_post  # Save back to database
 
     return {"message": "Successfully updated post", "updated_data": updated_post}
-@app.delete('/posts/{id}', status_code=204)
+
+
+@app.delete("/posts/{id}", status_code=204)
 def delete_post(id: int):
-    for index, data in enumerate(temporaryDataBase):
-        if data['id'] == id:
-            del temporaryDataBase[index]  # Safely delete the post
-            return {"message": f"Post with ID {id} has been deleted successfully"}
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(str(id)))
+    delete_post=cursor.fetchone()
+    conn.commit()
+    if delete_post==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"pst with id: {id} does not exist")
     
-    # If no post with the given ID is found
-    raise HTTPException(status_code=404, detail="Post not found")
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+@app.put('/posts/{id}')
+# put
+def UpdateById(id:int,post:Post):
+  cursor.execute("""UPDATE posts SET title = %s, content = %s , published = %s WHERE id = %s RETURNING * """,
+                 (post.title, post.content, post.published,str(id)))
+  updated_post= cursor.fetchone()
+  conn.commit()
+  if updated_post==None:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with {id} is not found ")
+  return {"data":updated_post}
