12e46af6edb89fe4a102b5ecd5617aa422f68064
pavankuamr14 <pavankumar14182@gmail.com>
2025-01-08 17:21:49 +0530

    ALL Curd operation are done with ORM sql alchemy
    detail description of the orm sql alchemy In the above code we have all the python that can communicate with database by performing the query to database. In the code spinned we have all the curd operations logic

--- app/database.py
+++ app/database.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = 'postgres://<username>:<password>@<ip-address/hostname>/<database_name>'
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password123@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password123@localhost/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 # connection to database with the session.
 # dependency.
--- app/main.py
+++ app/main.py
@@ -145,16 +145,20 @@ def testing(db: Session = Depends(get_db)):
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+
+    posts = db.query(models.Post).all()
     return {"data": posts}  # Return the fetched data
 
 
 @app.get("/posts/{id}")
-def getdatabyID(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    post = cursor.fetchone()
+def getdatabyID(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    # post = cursor.fetchone()
     if not post:
         raise HTTPException(
             status_code=404, detail=f"post with id : {id} Data was not found"
@@ -163,15 +167,25 @@ def getdatabyID(id: int):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def createPost(post: Post):
+def createPost(post: Post, db: Session = Depends(get_db)):
     # cursor.execute(f"{INSERT INTO posts (title,content,published) VALUES (post.title,post.content,post.published)}") --THIS WILL MAY CAUSE THE SQL ingestion TO AVOID THIS WE WILL USE THE BELOW COMMAND
 
-    cursor.execute(
-        """INSERT INTO posts(title,content,published) VALUES (%s,%s,%s) RETURNING * """,
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute(
+    #     """INSERT INTO posts(title,content,published) VALUES (%s,%s,%s) RETURNING * """,
+    #     (post.title, post.content, post.published),
+    # )
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    # new_post = models.Post(
+    #     title=post.title, content=post.content, published=post.published
+    # )
+    # OR #####
+
+    new_post = models.Post(**post.dict())
+
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"created data": new_post}
 
 
@@ -191,31 +205,39 @@ def updatePartialById(id: int, post: Post):
 
 
 @app.delete("/posts/{id}", status_code=204)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id)))
-    delete_post = cursor.fetchone()
-    conn.commit()
-    if delete_post == None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id)))
+    # delete_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"pst with id: {id} does not exist",
         )
-
+    post.delete(synchronize_session=False)
+    db.commit()
     raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 # put
-def UpdateById(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s , published = %s WHERE id = %s RETURNING * """,
-        (post.title, post.content, post.published, str(id)),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def UpdateById(id: int, post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """UPDATE posts SET title = %s, content = %s , published = %s WHERE id = %s RETURNING * """,
+    #     (post.title, post.content, post.published, str(id)),
+    # )
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_updated = post_query.first()
+    if post_updated == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with {id} is not found ",
         )
-    return {"data": updated_post}
+    # post_query.update({'title':'hey this is my updated title','content':'this is my updated content'},
+    #                   sychronize_session=False)
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
